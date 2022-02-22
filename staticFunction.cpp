bc20edd43e54fd9bb564394e4519f4e01237d3cc
Shivam5162323 <shivam5162323@gmail.com>
2022-02-22 23:42:54 +0530

    first commit

---
+++ staticFunction.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class emp
+{
+    int id;
+    static int count;
+
+public:
+    void setData(void)
+    {
+        cout << "Enter id " << endl;
+        cin >> id;
+        count++;
+    }
+    void getData(void)
+    {
+        cout << "ID of this employee is " << id << " and his employee number is " << count << endl;
+    }
+
+    static void  getCount(void){
+        cout<<"The value of count is "<<count;
+    }
+};
+
+int emp ::count = 1000; // if u only write "int emp :: count;" default value will be 0
+
+int main()
+{
+    emp harry, rohan, abhi;
+    // harry.id=32 ----> it will give error as id is private
+    // harry.count=32 ----> it will give error as count is private
+
+    harry.setData();
+    harry.getData();
+    emp :: getCount();
+
+    rohan.setData();
+    rohan.getData();
+    emp :: getCount();
+
+    abhi.setData();
+    abhi.getData();
+    emp :: getCount();
+
+    return 0;
+}
